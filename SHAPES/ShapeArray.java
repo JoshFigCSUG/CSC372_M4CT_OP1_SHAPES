47947aa361f67d2de96e592b519426a9dd9680c3
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-09-22 20:54:50 -0600

    Create ShapeArray.java

---
+++ SHAPES/ShapeArray.java
@@ -0,0 +1,13 @@
+public class ShapeArray {
+    public static void main(String[] args) {
+        Sphere sphere = new Sphere(5);
+        Cylinder cylinder = new Cylinder(3, 7);
+        Cone cone = new Cone(4, 6);
+
+        Shape[] shapeArray = {sphere, cylinder, cone};
+
+        for (Shape shape : shapeArray) {
+            System.out.println(shape.toString());
+        }
+    }
+}
